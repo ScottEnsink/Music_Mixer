5cd8202fc26f3b4bb2cfed027cc04d8a09ffde9a
Scott Ensink <scott.ensink@freedomdev.com>
2026-09-09 12:10:40 -0400

    Add TODO list for future improvements and update service worker cache version

--- public/sw.js
+++ public/sw.js
@@ -9,7 +9,7 @@
  * Bump CACHE_VERSION whenever you deploy, so old entries get cleaned out.
  */
 
-const CACHE_VERSION = "endless-v5";   // v5: Randomness split into Random interval + Random shift level
+const CACHE_VERSION = "endless-v9";   // v9: Evolution panel moved to top-left, last-updated bump
 
 const PRECACHE = [
   "./",
